1da52a66bd9175456703b29821681ec0b3262495
Z Wilder <zwilder@wsl-blog.com>
2023-10-13 00:22:42 -0700

    Added art for the Game Over screen
    The script generated art is cool - realized I could easily just do
    different colors in different layers, then combine the functions to get
    the composite image. The gameover screen does need to be tweaked - the
    way the game handles player death isn't really functional (but the art
    is nice!).

--- src/curses_engine.c
+++ src/curses_engine.c
@@ -241,7 +241,15 @@ void draw_help(void) {
     mvprintw(yoff + 22,xoff,"                           - Press any key to exit -                            ");
     unsetcolor(WHITE,BLACK);
     */
-    draw_art(ART_HELLO_WORLD);
+    draw_art(ART_TOMBSTONE);
+    int lvl = (g_mapcur->lvl + 1) * 10;
+    /* Player name at 20,12 - 38-12
+     * Level at 29,14 */
+    int xoff = 29 - (strlen(g_player->name) / 2);
+    char *depth = malloc(40 * sizeof(char));
+    snprintf(depth,40, "%dft", lvl);
+    curses_draw_msg(xoff,12,g_player->name);
+    curses_draw_msg(29,14,depth);
 }
 
 void draw_nwpl(void) {
